13b7d1db8d5d4bc18b1c654418e8cfaa47b8b56f
Link4Electronics <link4electronics@gmail.com>
2026-02-20 02:57:17 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -7,6 +7,7 @@ ARCH=$(uname -m)
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm    \
+    fluidsynth             \
     glm                    \
     help2man               \
     intltool               \
