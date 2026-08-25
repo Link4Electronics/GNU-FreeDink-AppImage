874b54fbf5bcac065fcb33f7c5ae75c1a073dd55
Link4Electronics <link4electronics@gmail.com>
2026-08-25 10:13:56 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -18,7 +18,3 @@ quick-sharun /usr/bin/freedink /usr/bin/freedink-dfarc /usr/bin/freedinkedit /us
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
-
-# Test the app for 12 seconds, if the app normally quits before that time
-# then skip this or check if some flag can be passed that makes it stay open
-quick-sharun --simple-test ./dist/*.AppImage
